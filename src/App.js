01e01300703e6f626a222d4a2c7c06bd78bdb1fc
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-01 18:12:49 -0500

    uso de una constante de datos iniciales para enviar a las componentes, tambien se puede usar los iteradores para la creacion de secciones

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
+import React from "react";
 
+const defalutTodos = [
+  { text: "cortar cebolla", completed: true },
+  { text: "Tomar el curso de introduccion a react.js", completed: false },
+  { text: "La la la la", completed: false },
+  { text: "La la la la Sol", completed: false },
+];
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
+      {/* <div className="App"> */}
       <TodoCounter total={16} completed={25} />
       <TodoSearch />
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defalutTodos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
       </TodoList>
       <CreateTodoButton />
-    </div>
+      {/* </div> */}
+    </React.Fragment>
   );
 }
 
