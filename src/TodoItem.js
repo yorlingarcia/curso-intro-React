b3bb6c2457f608234924cc508811074a9b603859
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-23 10:21:32 -0500

    creacion de un checkbox con mejor estilo

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,7 +2,10 @@ import "./TodoItem.css";
 function TodoItem(props) {
   return (
     <li className="item">
-      <input type="checkbox" checked={props.completed} />
+      <label className="prueba">
+        <input type="checkbox" checked={props.completed} className="i-prueba" />
+        <span className="s-prueba"></span>
+      </label>
       <p className={`p-item ${props.completed && "p-checked"}`}>{props.text}</p>
       <button className="button-item">X</button>
     </li>
