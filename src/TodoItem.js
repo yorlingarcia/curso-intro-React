cab3626f2dacb4549dda0316945b032366149693
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-23 09:45:42 -0500

    Uso de clases dinamicas

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ function TodoItem(props) {
   return (
     <li className="item">
       <input type="checkbox" checked={props.completed} />
-      <p className="p-item">{props.text}</p>
+      <p className={`p-item ${props.completed && "p-checked"}`}>{props.text}</p>
       <button className="button-item">X</button>
     </li>
   );
